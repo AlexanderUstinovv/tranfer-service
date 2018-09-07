ea5be7f71945f5c5517a61ac0d169198b13383d0
Alexander Ustinov <alexander.a.ustinov@gmail.com>
2018-09-07 17:20:58 +0500

    Added Account model

---
+++ src/main/java/com/revolut/transferservice/model/Account.java
@@ -0,0 +1,28 @@
+package com.revolut.transferservice.model;
+
+public class Account {
+
+    public Account(long id, double balance) {
+        this.id = id;
+        this.balance = balance;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    private long id;
+    private double balance;
+}
